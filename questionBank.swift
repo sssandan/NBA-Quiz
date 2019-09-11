13610bf1fde4d2859dd42fef3476a3981b12b9a8
Ryan Sandan <32689895+sssandan@users.noreply.github.com>
2019-09-10 18:08:43 -0700

    Create questionBank.swift

---
+++ questionBank.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+class QuestionBank
+{
+    var list = [Question]()
+    
+    init()
+    {
+        list.append(Question(text: "Patrick Ewing won a regular season MVP.", correctAnswer: false))
+        
+        list.append(Question(text: "Miami only missed the Playoffs 9 of their past 28 seasons.", correctAnswer: true))
+        
+        list.append(Question(text: "The Nets had more than 1 player make an All NBA 1st Team ever as a Net.", correctAnswer: false))
+        
+        list.append(Question(text: "John Wall is the only Washington Wizard to be in the Dunk Contest as a Washington Wizard.", correctAnswer: false))
+        
+        list.append(Question(text: "Giannis Antetokounmpo is the first ever Greek NBA MVP.", correctAnswer: true))
+        
+        list.append(Question(text: "The Philadelphia 76ers have as many division titles as Conference titles", correctAnswer: false))
+        
+        list.append(Question(text: "Pacers have been to the finals only once.", correctAnswer: false))
+        
+        list.append(Question(text: "Kobe only has 1 Regular Season MVP.", correctAnswer: true))
+        
+        list.append(Question(text: "Tim Duncan is the Spurs' leading scorer", correctAnswer: true))
+        
+        list.append(Question(text: "Kevin Durant played in Oklahoma his whole career before going to the Warriors", correctAnswer: false))
+        
+        list.append(Question(text: "The Warriors choked a 3 - 1 lead against the Cleveland Cavaliers in the 2016 NBA Finals", correctAnswer: true))
+        
+        list.append(Question(text: "Fred Vanvleet went undrafted.", correctAnswer: true))
+        
+        list.append(Question(text: "The Toronto Raptors weren't Toronto's first basketball team", correctAnswer: true))
+        
+        list.append(Question(text: "The Toronto Raptors won their first NBA championship against the Cleveland Cavaliers.", correctAnswer: false))
+        
+        list.append(Question(text: "Future Hall of Famer Pau Gasol went to medical school.", correctAnswer: true))
+        
+        list.append(Question(text: "The Toronto Raptors drafted Vince Carter.", correctAnswer: false))
+        
+        list.append(Question(text: "Shaquille Oneal once spent 1 million dollars in one day.", correctAnswer: true))
+        
+        list.append(Question(text: "The Golden State Warriors only lost one game after Klay Thompson signed a toaster given by a fan.", correctAnswer: true))
+        
+        list.append(Question(text: "David Stern was the NBA commissioner for 25 years.", correctAnswer: false))
+        
+        list.append(Question(text: "Kevin Durant was drafted by the Oklahoma City Thunder.", correctAnswer: false))
+        
+        list.append(Question(text: "Jump balls used to be after every basket.", correctAnswer: true))
+        
+        list.append(Question(text: "Lebron James, as of today, holds the record for most points, assists, rebounds, and triple doubles in the NBA finals.", correctAnswer: true))
+        
+        list.append(Question(text: "Stephen Curry is regarded to be one of the best shooters of all time.", correctAnswer: true))
+        
+        list.append(Question(text: "Damian Lillard's 3 point buzzer beater dagger against the Oklahoma City Thunder in the 2019 Playoffs caused Paul George to leave the Thunder for the Los Angeles Clippers.", correctAnswer: true))
+        
+        list.append(Question(text: "There are 30 NBA teams.", correctAnswer: true))
+        
+    }
+    
+}
